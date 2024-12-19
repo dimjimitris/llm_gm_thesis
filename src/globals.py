cad335cad9f89cbb6d422a1ff44827183fcb3ff0
ntua-el19005 <el19005@mail.ntua.gr>
2024-12-19 15:23:46 +0200

    added some variables and a game description

---
+++ src/globals.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+from enum import Enum
+
+# model id, set during game initialization
+MODEL_ID = None
+
+# load environment variables
+load_dotenv()
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+# check if environment variables are set
+if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY:
+    raise ValueError("AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set in a .env file")
+
+# game identifiers
+class GameId(Enum):
+    NEGOTIATION = 0
+    DICTATOR = 1
+    ROCK_PAPER_SCISSORS = 2
+
+# game id, set during game initialization
+GAME_ID = None
