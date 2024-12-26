738e22b10fdeb69290a50f008331003821447932
ntua-el19005 <el19005@mail.ntua.gr>
2024-12-26 18:57:48 +0200

    updated test file

--- test.py
+++ test.py
@@ -5,53 +5,60 @@ import games.rockpaperscissors as rps
 import json
 import os
 
-model_ids = [
+models = [
     {
         "model_id" : "anthropic.claude-3-5-sonnet-20241022-v2:0",
         "model_name" : "Claude 3.5 Sonnet v2",
     },
+    {
+        "model_id" : "mistral.mistral-large-2407-v1:0",
+        "model_name" : "Mistral Large (24.07)",
+    }
 ]
 
-def test_negotiation():
+def test_negotiation(idx):
+    model = models[idx]
     game = n.NegotiationGame(
         {"book" : 1, "hat" : 2, "ball" : 3},
         [
             {"book" : 1, "hat" : 3, "ball" : 1},
             {"book" : 2, "hat" : 1, "ball" : 2}
         ],
-        "comp",
-        model_ids[0]["model_id"],
-        log_path=os.path.join("logs", model_ids[0]["model_name"], "negotiation"),
+        "coop",
+        model["model_id"],
+        log_path=os.path.join("logs", model["model_name"], "negotiation"),
     )
     
     result = game.play()
     return result
 
-def test_rockpaperscissors():
+def test_rockpaperscissors(idx):
+    model = models[idx]
     game = rps.RockPaperScissorsGame(
         2,
         1,
         1,
         0,
-        model_ids[0]["model_id"],
-        log_path=os.path.join("logs", model_ids[0]["model_name"], "rockpaperscissors"),
+        model["model_id"],
+        log_path=os.path.join("logs", model["model_name"], "rockpaperscissors"),
     )
     result = game.play()
     return result
 
-def test_dictator_game():
+def test_dictator_game(idx):
+    model = models[idx]
     game = d.DictatorGame(
         [
             [-400, -900],
             [-401, -200],
         ],
         "minimize",
-        model_ids[0]["model_id"],
-        log_path=os.path.join("logs", model_ids[0]["model_name"], "dictator"),
+        model["model_id"],
+        log_path=os.path.join("logs", model["model_name"], "dictator"),
     )
     result = game.play()
     return result
 
 if __name__ == "__main__":
-    game_output = test_rockpaperscissors()
+    game_output = test_negotiation(1)
     print(json.dumps(game_output, indent=2))
