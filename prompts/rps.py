01668dd212c13bf42f471218db1cbb840d635268
dimjimitris <dimitrisgeor01@gmail.com>
2025-03-11 22:12:59 +0200

    fixed sequence of options

--- prompts/rps.py
+++ prompts/rps.py
@@ -5,8 +5,8 @@ RPS_PROMPTS = {
 Game Description:
 There are three possible moves: {rock}, {paper}, and {scissors}.
 - {rock} beats {scissors} giving the winner {r} points and the loser -{r} points.
-- {scissors} beats {paper} giving the winner {s} points and the loser -{s} points.
 - {paper} beats {rock} giving the winner {p} points and the loser -{p} points.
+- {scissors} beats {paper} giving the winner {s} points and the loser -{s} points.
 - If both players make the same move, the game is a tie and no points are awarded.
 
 In each round of the game you play against one other player. You and the other player each make a move privately. The moves are compared and you and the other player get your reward/penalty based on the moves made.
