99710560f5e127731ce47a26a9fe331f37139777
dimjimitris <dimitrisgeor01@gmail.com>
2025-07-03 13:19:35 +0300

    hello

--- play.py
+++ play.py
@@ -34,21 +34,21 @@ models = [
     #    "name" : "Claude 3.7 Sonnet",
     #    "thinking" : False,
     #},
-    #{
-    #    "id" : "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
-    #    "name" : "Claude 3.7 Sonnet (Thinking)",
-    #    "thinking" : True,
-    #},
+    {
+        "id" : "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+        "name" : "Claude 3.7 Sonnet (Thinking)",
+        "thinking" : True,
+    },
     #{
     #    "id" : "us.anthropic.claude-sonnet-4-20250514-v1:0",
     #    "name" : "Claude Sonnet 4",
     #    "thinking" : False,
     #},
-    #{
-    #    "id" : "us.anthropic.claude-sonnet-4-20250514-v1:0",
-    #    "name" : "Claude Sonnet 4 (Thinking)",
-    #    "thinking" : True,
-    #},
+    {
+        "id" : "us.anthropic.claude-sonnet-4-20250514-v1:0",
+        "name" : "Claude Sonnet 4 (Thinking)",
+        "thinking" : True,
+    },
     #{
     #    "id" : "meta.llama3-1-405b-instruct-v1:0",
     #    "name" : "Llama 3.1 405B Instruct",
@@ -59,16 +59,16 @@ models = [
     #    "name" : "Llama 3.3 70B Instruct",
     #    "thinking" : False,
     #},
-    {
-        "id" : "mistral.mistral-large-2407-v1:0",
-        "name" : "Mistral Large (24.07)",
-        "thinking" : False,
-    },
     #{
-    #    "id" : "us.deepseek.r1-v1:0",
-    #    "name" : "DeepSeek-R1",
+    #    "id" : "mistral.mistral-large-2407-v1:0",
+    #    "name" : "Mistral Large (24.07)",
     #    "thinking" : False,
     #},
+    {
+        "id" : "us.deepseek.r1-v1:0",
+        "name" : "DeepSeek-R1",
+        "thinking" : False,
+    },
 ]
 
 def trial_rps(
@@ -164,13 +164,7 @@ VALID_GAME_SETTINGS = [k for k in RPS_SETTINGS_COLLECTION.keys()]
 
 def main2_aux(iteration : int, self_consistency : bool) -> dict[str, list[Thread]]:
     threads : dict[str, list[Thread]] = {}
-    for model in [
-        {
-            "id" : "us.anthropic.claude-sonnet-4-20250514-v1:0",
-            "name" : "Claude Sonnet 4",
-            "thinking" : False,
-        },
-    ]:
+    for model in models:
         threads_list = list()
         #trial_idx = 200
         for valid_game_setting in VALID_GAME_SETTINGS:
@@ -418,4 +412,4 @@ def exec_threads(threads: list[Thread], count: int):
         time.sleep(5.0)
 
 if __name__ == "__main__":
-    main2_r("logs/logs_3", 24)
+    main2(5, False)
